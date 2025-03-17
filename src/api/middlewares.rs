2fbb3a6d4d65ab193836e799f25a96de8fc84af2
Steve <stevzhang01@gmail.com>
2025-03-18 02:14:40 +0800

    opt: code structure

--- src/api/middlewares.rs
+++ src/api/middlewares.rs
@@ -1,6 +1,6 @@
 use crate::api::response;
 use actix_web::Responder;
-use actix_web::body::{MessageBody};
+use actix_web::body::MessageBody;
 use actix_web::dev::ServiceResponse;
 use actix_web::http::StatusCode;
 use actix_web::middleware::ErrorHandlerResponse;
@@ -10,15 +10,13 @@ pub fn handle_err<B: MessageBody>(
 ) -> actix_web::Result<ErrorHandlerResponse<B>> {
     let (req, res) = res.into_parts();
     let message = res.error().map(|e| e.to_string()).unwrap_or_default();
-    let nrs = match res.status() {
-        StatusCode::NOT_FOUND => response::Response::<i32>::not_found().respond_to(&req),
-        StatusCode::BAD_REQUEST => {
-            response::Response::<i32>::params_error(message).respond_to(&req)
-        }
-        _ => response::Response::<i32>::internal_error(message).respond_to(&req),
+    let res = match res.status() {
+        StatusCode::NOT_FOUND => response::Response::<i8>::not_found().respond_to(&req),
+        StatusCode::BAD_REQUEST => response::Response::<i8>::params_error(message).respond_to(&req),
+        _ => response::Response::<i8>::internal_error(message).respond_to(&req),
     };
+    let res = res.map_into_boxed_body().map_into_right_body();
     Ok(ErrorHandlerResponse::Response(ServiceResponse::new(
-        req,
-        nrs.map_into_boxed_body().map_into_right_body(),
+        req, res,
     )))
 }
--- src/api/response.rs
+++ src/api/response.rs
@@ -21,6 +21,16 @@ pub struct Response<T> {
     data: Option<T>,
 }
 
+impl<T: Serialize> Responder for Response<T> {
+    type Body = BoxBody;
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
+
 impl<T> Response<T> {
     pub fn ok(data: Option<T>) -> Self {
         Self {
@@ -52,15 +62,5 @@ impl<T> Response<T> {
             message,
             data: None,
         }
-    }
-}
-
-impl<T: Serialize> Responder for Response<T> {
-    type Body = BoxBody;
-    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
-        let body = serde_json::to_string(&self).unwrap();
-        HttpResponse::Ok()
-            .content_type(ContentType::json())
-            .body(body)
     }
 }
